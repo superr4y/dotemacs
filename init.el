6c00baeed591e63bb27abad8b82419a563520b57
superr4y <superr4y@gmail.com>
2014-10-25 16:42:00 +0200

    init commit

---
+++ init.el
@@ -0,0 +1,23 @@
+(cua-mode 1)
+
+
+;; EVIL m0de settings
+(push "~/.emacs.d/evil" load-path)
+(require 'evil)
+(evil-mode 1) 
+
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (wombat)))
+ '(ido-mode (quote both) nil (ido)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
