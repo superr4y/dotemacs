304bad62e9866151d2617ec8a140b33a5b1b2113
Super Ray <superr4y@gmail.com>
2014-10-25 18:47:20 +0200

    disable menu and tool bar

--- init.el
+++ init.el
@@ -1,10 +1,8 @@
+;; General settings
 (cua-mode 1)
+(tool-bar-mode 0)
+(menu-bar-mode 0)
 
-
-;; EVIL m0de settings
-(push "~/.emacs.d/evil" load-path)
-(require 'evil)
-(evil-mode 1) 
 
 
 
@@ -21,3 +19,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+
+;; Plugin Settings
+
+;; evil mode 
+(push "~/.emacs.d/evil" load-path)
+(require 'evil)
+(evil-mode 1) 
+
